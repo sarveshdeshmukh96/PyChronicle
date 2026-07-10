66ab1a82138743d54d997e92a0645cb5a360b65d
Yaduraj21 <yadurajsy@gmail.com>
2026-07-10 18:42:52 +0530

    databse.py is initiated

---
+++ src/database.py
@@ -0,0 +1,29 @@
+import sqlite3
+import json
+from datetime import datetime
+
+class Database:
+    def __init__(self, db_path: str = "pychronicle.db"):
+        self.connection = sqlite3.connect(db_path)
+        self.cursor = self.connection.cursor()
+        self.create_tables()
+    
+    def create_tables(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS sessions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                file_name TEXT,
+                run_at TEXT
+            )
+        """)
+        self.cursor.execuute("""
+            CREATE TABLE IF NOT EXISTS events (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                session_id INTEGER,
+                line_no INTEGER,
+                event_type TEXT,
+                variables TEXT,
+                FOREIGN KEY (session_id) REFERENCES session(id)
+                )
+            """)
+        self.connection.commit()
